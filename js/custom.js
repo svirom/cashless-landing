275c9c315b749592ddad5186292978da2b89e6ba
svirom <svirom@yahoo.com>
2020-08-05 20:26:45 +0300

    translate, smooth scroll

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
 
+  $(".nav-link").on('click', function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      //window.location.hash = hash;
+    });
+    
+  });
 
 })
